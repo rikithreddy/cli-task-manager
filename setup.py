2a7a24a76e6ad51e02e3732d73ceafd8341d7f52
Rikith <rikith.legend@gmail.com>
2019-02-10 12:23:07 +0530

    Added Setup file for the app

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''
+Setup file for cli-task-manager installs all dependencies 
+and converts application in to a command.
+'''
+from setuptools import setup
+
+setup(
+	name="cli-task",
+	version=1.0,
+	py_modules=['hello'],
+	install_requires=['Click'],
+	entry_points='''
+		[console_scripts]
+		clitask=clitask:run
+		'''
+	)
