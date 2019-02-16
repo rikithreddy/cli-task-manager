e23a9f96066523ab7d475434a33ec060cd0d1aac
Rikith <rikith.legend@gmail.com>
2019-02-16 21:36:22 +0530

    Added comments to the init code

--- lib/utils.py
+++ lib/utils.py
@@ -1,10 +1,15 @@
 from os import mkdir, path, makedirs, utime
+
+# Global constants required for the application
 BASE_DIR = "clitask/"
 DATA_DIR = path.join(BASE_DIR , 'data')
 CONFIG_DIR = path.join(BASE_DIR , 'config')
 DATA_FILE = path.join(DATA_DIR , 'task.csv')
 
+
+# The function initialises all the nessessary directories required by the application
 def init():
+
 	if not path.exists(BASE_DIR):
 		makedirs(BASE_DIR)
 
